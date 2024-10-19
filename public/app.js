6e95db76fdab86b1717347d5f6f480437ed49f75
Omkar-Ardekar <omkarardekar77@gmail.com>
2024-10-19 17:04:52 +0530

    Adding files

---
+++ public/app.js
@@ -0,0 +1,8 @@
+let reg = document.querySelector("#reg");
+let guest = document.querySelector("#guest");
+reg.addEventListener("click", function () {
+  window.location.href = "register.html";
+});
+guest.addEventListener("click", function () {
+  window.location.href = "layout.html";
+});
---
+++ public/layout.js
@@ -0,0 +1,48 @@
+// let createBtn = document.querySelector("#self-profile");
+// let popupInp = document.querySelector(".popup-input");
+// let container = document.querySelector(".container");
+// let crossBtn = document.querySelector(".cross");
+// let main = document.querySelector("main");
+
+// createBtn.addEventListener("click", function () {
+//   container.classList.add("blur");
+//   popupInp.classList.remove("hidden");
+// });
+
+// crossBtn.addEventListener("click", function () {
+//   popupInp.classList.add("hidden");
+//   container.classList.remove("blur");
+// });
+
+// let role = document.querySelector("#role");
+// let userName = document.querySelector("#userName");
+// let field = document.querySelector("#field");
+// let subjects = document.createElement("input");
+// subjects.setAttribute("name", "sub");
+// subjects.setAttribute("placeholder", "Enter your teaching Subjects");
+// let profile = document.querySelector(".profile");
+// role.addEventListener("change", function () {
+//   subjects.classList.add("input-item");
+//   if (this.value === "Teacher") {
+//     profile.appendChild(subjects);
+//   } else {
+//     subjects.remove();
+//   }
+// });
+
+// let submitBtn = document.querySelector("#submit-btn");
+// submitBtn.addEventListener("click", function () {
+//   setTimeout(function () {
+//     popupInp.classList.add("hidden");
+//     container.classList.remove("blur");
+//     userName.value = "";
+//     field.value = "";
+//     role.value = "";
+//     subjects.value = "";
+//   }, 100);
+// });
+
+// let search = document.querySelector("#search-input");
+// setTimeout(() => {
+//   search.setAttribute("placeholder", "Search here ...");
+// }, 5000);
